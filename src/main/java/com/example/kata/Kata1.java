d2d058c5468abd12abc61f77069557b7ce118df2
topl <l.top@elsevier.com>
2023-07-18 11:44:12 +0200

    Added test for kata1 exercise1

--- src/main/java/com/example/kata/Kata1.java
+++ src/main/java/com/example/kata/Kata1.java
@@ -1,2 +1,12 @@
-package com.example.kata;public class Kata1 {
+package com.example.kata;
+
+import lombok.Builder;
+
+@Builder
+public record Kata1(Integer number, String numberAsString) {
+
+public String convertIntegerToString(Integer number){
+  return number.toString();
+}
+
 }
