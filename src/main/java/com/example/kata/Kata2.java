835cbf5d0ae5b44ddfb092affb3e1620d8f53ab2
topl <l.top@elsevier.com>
2023-07-18 15:52:20 +0200

    changed add method to handle newlines as seperators

--- src/main/java/com/example/kata/Kata2.java
+++ src/main/java/com/example/kata/Kata2.java
@@ -2,7 +2,6 @@ package com.example.kata;
 
 import java.util.Arrays;
 import java.util.Optional;
-
 import java.util.stream.Stream;
 
 public record Kata2() {
@@ -10,7 +9,7 @@ public record Kata2() {
   public int add(String... numbers) {
     return Optional.of(Stream.of(numbers)
             .filter(stream -> !stream.isEmpty())
-            .flatMapToInt(stringNumber -> Arrays.stream(stringNumber.split(","))
+            .flatMapToInt(stringNumber -> Arrays.stream(stringNumber.split("\s*[,\n]\s*"))
                 .mapToInt(Integer::valueOf))
             .sum())
         .orElse(0);
