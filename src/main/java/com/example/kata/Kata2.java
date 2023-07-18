4dc811c026d995cedd595e4f97fe52310736d015
topl <l.top@elsevier.com>
2023-07-18 18:05:21 +0200

    refactored peek method

--- src/main/java/com/example/kata/Kata2.java
+++ src/main/java/com/example/kata/Kata2.java
@@ -9,22 +9,23 @@ import java.util.stream.Stream;
 public record Kata2() {
 
   public int add(String... numbers) throws IllegalArgumentException {
-    return Optional.of(checkArgumentsValidity(numbers)
+
+    return Optional.of(Stream.of(numbers)
+            .filter(stream -> !stream.isEmpty())
+            .flatMap(checkIfValueEndsWithComma())
             .flatMapToInt(getSeparatedNumbers())
             .sum())
         .orElse(0);
   }
 
-
-  private static Stream<String> checkArgumentsValidity(String[] numbers) {
-    return Stream.of(numbers)
-        .filter(stream -> !stream.isEmpty())
-        //FIXME: replace peek method
-        .peek(stringNumber -> {
-          if (stringNumber.endsWith(",")) {
-            throw new IllegalArgumentException();
-          }
-        });
+  private static Function<String, Stream<String>> checkIfValueEndsWithComma() {
+    return stringNumber -> {
+      if (stringNumber.endsWith(",")) {
+        throw new IllegalArgumentException();
+      } else {
+        return Stream.of(stringNumber);
+      }
+    };
   }
 
   private static Function<String, IntStream> getSeparatedNumbers() {
