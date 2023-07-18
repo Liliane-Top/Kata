047506c4434cc202e430a0cb415f0255a8d808fa
topl <l.top@elsevier.com>
2023-07-18 12:12:54 +0200

    added test and code to convert number as multiple of 5 and return the string Buzz

--- src/main/java/com/example/kata/Kata1.java
+++ src/main/java/com/example/kata/Kata1.java
@@ -8,9 +8,13 @@ public record Kata1(Integer number, String numberAsString) {
   public String convertIntegerToString(Integer number) {
     if (number % 3 == 0) {
       return "Fizz";
-    } else {
-      return number.toString();
     }
+    if (number % 5 == 0) {
+      return "Buzz";
+    }
+
+    return number.toString();
+
   }
 
 }
