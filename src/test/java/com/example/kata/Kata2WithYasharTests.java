0b8f2f35b9e6744393002912f88fe83c7ed87182
topl <l.top@elsevier.com>
2023-07-27 11:35:11 +0200

    Added test for string with 2

--- src/test/java/com/example/kata/Kata2WithYasharTests.java
+++ src/test/java/com/example/kata/Kata2WithYasharTests.java
@@ -29,7 +29,8 @@ public class Kata2WithYasharTests {
     return Stream.of(
         Arguments.of("", 0),
         Arguments.of("1", 1),
-        Arguments.of("1,2", 3));
+        Arguments.of("1,2", 3),
+        Arguments.of("2", 2));
   }
 
   //1. The method can take up to two numbers, separated by commas, and will return their sum as a result. So the inputs can be: “”, “1”, “1,2”. For an empty string, it will return 0.
