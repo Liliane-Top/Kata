591b966ba42436a8b7d28f674ce0780c7deef991
topl <l.top@elsevier.com>
2023-07-27 16:33:55 +0200

    added test

--- src/main/java/com/example/kata/Calculator.java
+++ src/main/java/com/example/kata/Calculator.java
@@ -2,13 +2,15 @@ package com.example.kata;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 
 public class Calculator {
 
-  public Integer add(String input) throws Exception {
+  public Integer add(String input) throws IllegalArgumentException {
 
     Pair<List<String>, String> parsed = parse(input);
 
@@ -21,16 +23,33 @@ public class Calculator {
     if (numbers.isEmpty()) {
       return 0;
     }
-    if (numbers.matches(".*[^0-9]")) {
-      throw new Exception("Input can't end with '" + input.charAt(input.length()-1) + "'");
+    if (numbers.matches(".*\\D")) {
+      throw new IllegalArgumentException(
+          "Input can't end with '" + input.charAt(input.length() - 1) + "'");
     }
     return tokenize(delim, numbers).sum();
   }
 
   private static IntStream tokenize(List<String> delim, String numbers) {
+    AtomicInteger index = new AtomicInteger(1);
     return Arrays.stream(numbers.split(delim.toString()))
         .filter(s -> !s.isEmpty())
+        .flatMap(str -> checkIfOnlyNumbers(delim, index, str))
         .mapToInt(Integer::parseInt);
+  }
+
+  private static Stream<String> checkIfOnlyNumbers(List<String> delim, AtomicInteger index,
+      String str) {
+    try {
+      index.getAndIncrement();
+      Integer.parseInt(str);
+      return Stream.of(str);
+    } catch (NumberFormatException exception) {
+      String invalid = Arrays.stream(str.split("")).filter(s -> !StringUtils.isNumeric(s))
+          .findFirst().orElse("");
+      throw new IllegalArgumentException(
+          "'" + delim.get(0) + "' expected but '" + invalid + "' found at position " + index);
+    }
   }
 
   private Pair<List<String>, String> parse(String input) {
--- src/test/java/com/example/kata/Kata2WithYasharTests.java
+++ src/test/java/com/example/kata/Kata2WithYasharTests.java
@@ -57,7 +57,7 @@ public class Kata2WithYasharTests {
     return Stream.of(
         Arguments.of("1,2,", "Input can't end with ','"),
         Arguments.of("1,2,\n", "Input can't end with '\n'"),
-        Arguments.of("//|\n1|2,3", "‘|’ expected but ‘,’ found at position 3."));
+        Arguments.of("//|\n1|2,3", "'|' expected but ',' found at position 3"));
   }
 
 }
