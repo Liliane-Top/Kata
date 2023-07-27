0d6fa6af35a722ba5bf84a13dc0ad9f7091317b3
Yashar Shahi <y.shahi@elsevier.com>
2023-07-27 11:32:22 +0200

    pass

--- src/main/java/com/example/kata/Calculator.java
+++ src/main/java/com/example/kata/Calculator.java
@@ -3,8 +3,6 @@ package com.example.kata;
 public class Calculator {
 
     public Integer add(String input) {
-        if (input.length() > 0)
-            return 1;
-        return 0;
+        return input.length();
     }
 }
