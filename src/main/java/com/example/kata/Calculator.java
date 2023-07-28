564dbba70901c07507f0eea9a5d4ff83f4bd298e
topl <l.top@elsevier.com>
2023-07-28 15:31:42 +0200

    refactor

--- src/main/java/com/example/kata/Calculator.java
+++ src/main/java/com/example/kata/Calculator.java
@@ -9,7 +9,6 @@ import org.apache.commons.lang3.tuple.Pair;
 
 public class Calculator {
 
-
   public Integer add(String input) throws IllegalArgumentException {
     Pair<List<String>, String> parsed = parse(input);
     List<String> delim = parsed.getLeft();
@@ -24,30 +23,31 @@ public class Calculator {
     Stream<Integer> validInput = tokenize(delim, numbers)
         .flatMap(str -> checkIfValueIsANumber(delim, str, index));
 
-    List<Integer> negativeNumbers = validInput.flatMap(number -> checkIfNumberIsNegative(number))
-        .toList();
+    checkIfValueIsAPositiveNumber(validInput);
 
+    Stream<Integer> validInput2 = tokenize(delim, numbers)
+        .flatMap(str -> checkIfValueIsANumber(delim, str, index));
+
+    return validInput2.reduce(0, Integer::sum);
+  }
+
+  private void checkIfValueIsAPositiveNumber(Stream<Integer> validInput) {
+    List<Integer> negativeNumbers = validInput.filter(number -> number< 0).toList();
 
     if (!negativeNumbers.isEmpty()) {
       String negatives = negativeNumbers.get(0).toString();
       for (int i = 1; i < negativeNumbers.size(); i++) {
-        negatives = negatives.concat( "," + negativeNumbers.get(i));
+        negatives = negatives.concat("," + negativeNumbers.get(i));
       }
       throw new IllegalArgumentException(
           String.format("Negative number(s) not allowed: %s", negatives));
     }
-    Stream<Integer> validInput2 = tokenize(delim, numbers)
-        .flatMap(str -> checkIfValueIsANumber(delim, str, index));
-
-
-    return validInput2.reduce(0, Integer::sum);
   }
 
   private Stream<String> tokenize(List<String> delim, String numbers) {
     return Arrays.stream(numbers.split(delim.toString()))
         .filter(s -> !s.isEmpty());
   }
-
 
   private Stream<Integer> checkIfValueIsANumber(List<String> delim, String str,
       AtomicInteger index) {
@@ -62,15 +62,6 @@ public class Calculator {
               delim.get(0), invalidChar, index));
     }
   }
-
-  private Stream<Integer> checkIfNumberIsNegative(Integer number) {
-    if (number < 0) {
-      return Stream.of(number);
-    } else {
-      return Stream.empty();
-    }
-  }
-
   private Pair<List<String>, String> parse(String input) {
     List<String> delim = List.of(",", "\n");
     if (input.startsWith("//")) {
