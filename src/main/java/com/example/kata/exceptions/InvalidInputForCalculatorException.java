33502be5c1075ee50b45013d07d30c6d46ca5618
topl <l.top@elsevier.com>
2023-07-29 15:42:06 +0200

    refactor

--- src/main/java/com/example/kata/exceptions/InvalidInputForCalculatorException.java
+++ src/main/java/com/example/kata/exceptions/InvalidInputForCalculatorException.java
@@ -6,24 +6,24 @@ import lombok.Getter;
 @Getter
 public class InvalidInputForCalculatorException extends Exception {
 
-  private String message;
+  private final String message;
 
   public InvalidInputForCalculatorException(List<String> messages) {
     this.message = printErrorMessage(messages);
   }
 
   public String printErrorMessage(List<String> messages) {
-    StringBuilder message = new StringBuilder();
+    StringBuilder combinedMessage = new StringBuilder();
 
     for (String errorMessage : messages) {
-      if (message.isEmpty()) {
-        message.append(errorMessage);
-      } else if (message.toString().contains("Negative")) {
-        message.append("," + errorMessage.substring(31));
+      if (combinedMessage.isEmpty()) {
+        combinedMessage.append(errorMessage);
+      } else if (combinedMessage.toString().contains("Negative")) {
+        combinedMessage.append(",").append(errorMessage.substring(31));
       } else if (messages.size() >1) {
-        message.insert(0,errorMessage + "\n");
+        combinedMessage.insert(0,errorMessage + "\n");
       }
     }
-    return message.toString();
+    return combinedMessage.toString();
   }
 }
