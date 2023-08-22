37f9153587c5dd9725f7db447bb952419feafa6e
topl <l.top@elsevier.com>
2023-08-22 11:28:51 +0200

    tested manually

--- src/main/java/com/example/kata/BarcodeScanner.java
+++ src/main/java/com/example/kata/BarcodeScanner.java
@@ -1,39 +1,54 @@
 package com.example.kata;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BarcodeScanner {
-  //  Scanner in = new Scanner(System.in);
-  //
-  //        String s = in.nextLine();
-  //        System.out.println("You entered string " + s);
-  //
-  //        int a = in.nextInt();
-  //        System.out.println("You entered integer " + a);
-  //
-  //        float b = in.nextFloat();
-  //        System.out.println("You entered float " + b);
+  public static List<String> prices = new ArrayList<>();
+
 
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+
+    Scanner in = new Scanner(System.in);
     System.out.println("Please enter a barcode: ");
 
-    String barcode = input.nextLine();
-    System.out.println("You entered the following barcode " + barcode);
 
-    System.out.println("The price is " + scanBarcode(barcode));
+    do{
+      String input = in.nextLine();
+      if(input.equals("stop")) break;
+      System.out.println("You entered the following: " + input);
 
+      System.out.println(scanBarcode(input) + "\n");
+      System.out.println("Please enter a barcode: ");
+    }
+    while (in.hasNextLine());
 
+    in.close();
+  }
 
+  private static String getTotal(List<String> prices) {
+    double total = 0.0;
+    for(String price: prices){
+      total += Double.parseDouble(price);
+    }
+    return "Total amount is $" + total;
   }
 
   public static String scanBarcode(String barcode){
 
     return switch (barcode) {
-      case "12345" -> "$7.25";
-      case "23456" -> "$12.50";
+      case "12345" -> {
+         prices.add("7.25");
+         yield "The price is $7.25";
+      }
+      case "23456" -> {
+        prices.add("12.50");
+        yield  "The price is $12.50";
+      }
       case "99999" -> "barcode not found";
       case "" -> "empty barcode";
+      case "total" -> getTotal(prices);
       default -> "unknown";
     };
 
