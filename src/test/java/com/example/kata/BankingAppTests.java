0c97f1d92279a06ce3e1d991cbe1468f74a09201
topl <l.top@elsevier.com>
2023-09-14 08:43:30 +0200

    added test and passed the test

--- src/test/java/com/example/kata/BankingAppTests.java
+++ src/test/java/com/example/kata/BankingAppTests.java
@@ -1,29 +1,52 @@
 package com.example.kata;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class BankingAppTests {
 
-  Account account = new Account(0);
 
-  @ParameterizedTest
-  @MethodSource("happyFlow")
-  void bankingApp_happyFlow(Integer amount, Integer balance) {
-    account.deposit(amount);
+  private static Account account;
 
-    Assertions.assertTrue(account.getBalance().equals(balance));
-
-
+  @BeforeAll
+  static void setup(){
+    account = new Account(0);
   }
 
-  static Stream<Arguments> happyFlow() {
-    return Stream.of(
-        Arguments.of(500, 500)
-    );
+//  @ParameterizedTest
+//  @MethodSource("happyFlow")
+//  void bankingApp_happyFlow(Integer amount, Integer balance) {
+//
+//    assertTrue(account.getBalance().equals(balance));
+//
+//
+//  }
+//
+//  static Stream<Arguments> happyFlow() {
+//    return Stream.of(
+//        Arguments.of(500, 500),
+//        Arguments.of()
+//    );
+//  }
+
+  @Test
+  void call_deposit_check_balance(){
+    account.deposit(500);
+    assertTrue(account.getBalance().equals(500));
   }
 
 
